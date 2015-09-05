442a5f7104c7426b76e28e967c4f6af99e3382d2
airwindh <airwindh25@gmail.com>
2015-09-05 20:12:24 +0530

    Commit Message

--- getFile.php
+++ getFile.php
@@ -4,37 +4,36 @@ if(!(isset($_SESSION['logggedin']) && ($_SESSION['logggedin']==1)))
 {
 	die("you are not logged in");
 }
-
 $uid = $_SESSION['uid'];
-//$sid = $_SESSION['sid'];
+$sid = $_SESSION['sid'];
 $qid = $_SESSION['qid'];
-$uploadedFile = $_SESSION['programFilePath'];
-
-$command = system("g++ ".$uploadedFile." 2>out.txt");
+//$uploadedFile = fopen("$_SESSION['programFilePath']","r+");//$_SESSION[''];
+$orgFile = fopen("$_SESSION['programFilePath']","a+");
+system("g++ ".$orgFile." 2>out.txt");
 $errorFile = "o_".$uid."_".$qid."_".$_SESSION['attempts']."txt";
 $logFile = fopen("$errorFile","w+");
 $outFile = fopen("out.txt","r+");
-$orgFile = fopen("$uploadedFile","r+");
-$eCount;
-$lCount;
+$eCount=0;
+$lCount=0;
 while(!feof($outFile))
 {
 	$line = fgets($outFile);
-	if(strpos($line,'error')!=false)
+	if((strpos($line,'error')!=false))
 	$eCount++;	
 }
 fclose($outFile);
 while(!feof($orgFile))
 {
 	$line = fgets($orgFile);
-	if(strlen($)>0)
-		$lcount++;
+	if(strlen($line)>0)
+		$lCount++;
 }
-fclose($orgfile);
+fclose($orgFile);
 $result = $lCount."<br>".$eCount;
 fwrite($logFile,$result);
 fclose($logFile);
 $_SESSION['attempts'] = $_SESSION['attempts']+1;
 include('db.php');
-$result = mysqli_query($conn,"INSERT INTO submissions(uid,qid,attempts,errors,lines_used) values($uid,$qid,$_SESSION['attempts'],$eCount,$lCount)");
-echo if(!$result)
+$result = mysqli_query($conn,"INSERT INTO submissions(uid,qid,attempts,errors,lines_used) VALUES($uid,$qid,1,$eCount,$lCount)");
+echo $result;
+?>
